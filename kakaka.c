da3ae266d31b15f69f8c60a5c9aa965e13a8648f
Gabriel-coding-del <gabrielsirbu0012@gmail.com>
2025-04-23 11:22:24 +0300

    Add files via upload

---
+++ kakaka.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	for(int i = 0; i <= 15; i++){
+		printf("#");
+	}
+	printf("\n");
+	for (int i = 0; i <= 13; i++){
+		printf("#              #\n");
+	}
+	for(int i = 0; i <= 15; i++){
+		printf("#");
+	}
+	printf("\n");
+
+	return 0;
+}
